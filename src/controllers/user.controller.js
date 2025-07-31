84691a663035ac2f3dfd486c6a7b457a32bebf39
darshankalburgi <148205043+darshankalburgi@users.noreply.github.com>
2025-07-31 14:36:27 +0530

    fix all register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,11 @@
 import { asyncHandler } from '../utils/asyncHandler.js';
 import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
-import { uploadCloudinary } from "..utils/cloudinary.js";
-import { ApiResponse } from "..utils/ApiResponse.js";
+import { uploadCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler( async (req, res) => {
+
    // get the input from frontend
    // validate the input - not empty
    // check if user already exist 
@@ -17,8 +18,7 @@ const registerUser = asyncHandler( async (req, res) => {
 
     //get input from frontend
     const {fullName,email,username,password}= req.body
-    console.log("email:", email);
-
+    
     //validation
    if (
     [fullName,email,username,password].some((field) => 
@@ -28,7 +28,7 @@ const registerUser = asyncHandler( async (req, res) => {
    }
 
    //check if user already exists
-   const existedUser = User.findOne({
+   const existedUser = await User.findOne({
     $or: [{ username },{ email }]
    })
 
@@ -38,9 +38,18 @@ const registerUser = asyncHandler( async (req, res) => {
 
    //check for images 
 
+   //console.log(req.files);
+
    const avatarLocalPath = req.files?.avatar[0]?.path;
-   const coverImageLocalPath = req.files?.coverImage[0]?.path;
-   path;
+    //const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.
+      coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImages[0].path
+      }
+   
+    
 
    if (!avatarLocalPath) {
     throw new ApiError (400, "Avatar file is required")
@@ -83,4 +92,27 @@ const registerUser = asyncHandler( async (req, res) => {
 
 })
 
-export { registerUser };
+export { registerUser };     
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
