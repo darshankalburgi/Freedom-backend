9eb8381e935adbfd77f6bd783194a1090d0165cc
darshankalburgi <148205043+darshankalburgi@users.noreply.github.com>
2025-08-05 22:00:55 +0530

    fix: correct cover image upload route path in user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -47,14 +47,14 @@ router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvat
 router.route("/cover-image")
 .patch(
   verifyJWT, 
-  upload.single("/coverImage"), 
+  upload.single("coverImage"), 
   updateUserCoverImage) 
 
 router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
 
 router.route("/history").get(verifyJWT, getWatchHistory)
 
-
+ 
 
 export default router;
 
